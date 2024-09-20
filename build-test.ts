f62da47c94e26b733cc075b8aee32f56d6410de7
dan-schel <me@danschellekens.com>
2024-09-20 21:03:15 +1000

    build html from multiple files

---
+++ build-test.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import fsp from "fs/promises";
+
+const sourceDir = "test/src";
+const destDir = "test/html";
+const indexFile = "test/index.html";
+const keyword = "        {{SECTIONS_GO_HERE}}";
+
+async function main() {
+  if (fs.existsSync(destDir)) {
+    await fsp.rmdir(destDir, { recursive: true });
+  }
+
+  await fsp.mkdir(destDir);
+
+  const files = (await fsp.readdir(sourceDir)).filter((file) =>
+    file.endsWith(".html"),
+  );
+
+  const html = await Promise.all(
+    files.map(async (file) => {
+      return await fsp.readFile(`${sourceDir}/${file}`, "utf-8");
+    }),
+  );
+
+  const formattedHtml = html
+    .join("\n")
+    .split("\n")
+    .map((line) => `        ${line}`)
+    .join("\n");
+
+  const indexHtml = await fsp.readFile(indexFile, "utf-8");
+
+  const outHtml = indexHtml.replace(keyword, formattedHtml);
+
+  await fsp.writeFile(`${destDir}/index.html`, outHtml);
+}
+
+main();

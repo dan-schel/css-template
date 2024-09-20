4af180fcee276f9b918ac31f315f762e2c6ad0b3
dan-schel <me@danschellekens.com>
2024-09-20 21:31:35 +1000

    include scss files dynamically too

--- build-test.ts
+++ build-test.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import fsp from "fs/promises";
 import { promisify } from "util";
 import { exec as execCallback } from "child_process";
+import { relative } from "path";
 
 const exec = promisify(execCallback);
 
@@ -9,21 +10,38 @@ const sourceDir = "test/src";
 const destDir = "test/dist";
 const indexFile = "test/index.html";
 const indexCssFile = "test/index.scss";
-const keyword = "        {{SECTIONS_GO_HERE}}";
+const keywordHtml = "        <!-- {{INSERT_HERE}} -->";
+const keywordCss = "// {{INSERT_HERE}}";
 
 async function main() {
+  try {
+    await compile();
+  } catch (e) {
+    console.warn("❌ Compile error. Will retry on next change.");
+    if ("stderr" in e) {
+      console.warn(e.stderr);
+    } else {
+      console.warn(e);
+    }
+  }
+}
+
+async function compile() {
   if (fs.existsSync(destDir)) {
-    await fsp.rmdir(destDir, { recursive: true });
+    await fsp.rm(destDir, { recursive: true });
   }
 
   await fsp.mkdir(destDir);
 
-  const files = (await fsp.readdir(sourceDir)).filter((file) =>
+  const htmlFiles = (await fsp.readdir(sourceDir)).filter((file) =>
     file.endsWith(".html"),
+  );
+  const cssFiles = (await fsp.readdir(sourceDir)).filter((file) =>
+    file.endsWith(".scss"),
   );
 
   const html = await Promise.all(
-    files.map(async (file) => {
+    htmlFiles.map(async (file) => {
       return await fsp.readFile(`${sourceDir}/${file}`, "utf-8");
     }),
   );
@@ -33,14 +51,23 @@ async function main() {
     .split("\n")
     .map((line) => `        ${line}`)
     .join("\n");
-
   const indexHtml = await fsp.readFile(indexFile, "utf-8");
-
-  const outHtml = indexHtml.replace(keyword, formattedHtml);
-
+  const outHtml = indexHtml.replace(keywordHtml, formattedHtml);
   await fsp.writeFile(`${destDir}/index.html`, outHtml);
 
-  await exec(`sass ${indexCssFile} ${destDir}/index.css`);
+  const formattedCssImports = cssFiles
+    .map(
+      (x) =>
+        `@import "${relative(destDir, sourceDir)}/${x.replace(".scss", "")}";`,
+    )
+    .join("\n");
+  const indexCss = await fsp.readFile(indexCssFile, "utf-8");
+  const outCss = indexCss.replace(keywordCss, formattedCssImports);
+  await fsp.writeFile(`${destDir}/index.scss`, outCss);
+
+  await exec(`sass ${destDir}/index.scss ${destDir}/index.css`);
+
+  console.log("✅ Compiled.");
 }
 
 main();
